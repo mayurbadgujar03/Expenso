97379285bab2bacf16eec4f672091c3336fb5545
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-01 18:51:25 +0530

    added a pre-save hook for hashing and a method for checking the password

--- src/models/Users.models.js
+++ src/models/Users.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
   {
@@ -30,4 +31,13 @@ const userSchema = new Schema(
   },
 );
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
 export const User = mongoose.model("User", userSchema);
