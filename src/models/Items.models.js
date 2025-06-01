07bd132764e313d16c71fc5922666294f1065280
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-01 17:34:59 +0530

    Item model added in /src/models

--- src/models/Items.models.js
+++ src/models/Items.models.js
@@ -1,5 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
-const itemSchema = Schema({}, {});
+const itemSchema = new Schema(
+    {
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: {
+                url: String,
+                localpath: String,
+            },
+            default: {
+                url: `https://placehold.co/600x400`,
+                localpath: "",
+            }
+        }
+    }, 
+    {
+        timestamps: true,
+    }
+);
 
 export const Item = mongoose.model("Item", itemSchema);
