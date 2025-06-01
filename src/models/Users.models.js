a74f180d7e2d89b1a81cace6847a6f63ee5a1177
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-01 17:04:19 +0530

    User model added in /src/models

--- src/models/Users.models.js
+++ src/models/Users.models.js
@@ -1,5 +1,32 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
-const userSchema = Schema({}, {});
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            lowercase: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    }, 
+    {
+        timestamps: true,
+    });
 
 export const User = mongoose.model("User", userSchema);
