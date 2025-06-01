7c0fa024d53317000c3e0a3bf3785f3f58d999ad
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-01 17:44:09 +0530

    Purchase model added in /src/models

--- src/models/Purchases.models.js
+++ src/models/Purchases.models.js
@@ -1,5 +1,27 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
-const purchasesSchema = Schema({}, {});
+const purchasesSchema = new Schema(
+    {
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        item: {
+            type: Schema.Types.ObjectId,
+            ref: "Item",
+            required: true,
+        },
+        quantity: {
+            type: String,
+        },
+        total_price: {
+            type: String,
+        }
+    }, 
+    {
+        timestamps: true,
+    }
+);
 
 export const Purchases = mongoose.model("Purchases", purchasesSchema);
