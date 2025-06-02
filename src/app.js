26c18335fe525515c689bfac8878e4432984727b
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-03 01:08:48 +0530

    Added login functionality, Used JWT tokens, Used Cookie Parser

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -6,6 +7,7 @@ const app = express();
 import userRegister from "./routes/auth.routes.js";
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/users", userRegister);
 
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,6 +2,9 @@ import { ApiError } from "../utils/api-error.js";
 import { User } from "../models/Users.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const registerUser = async (req, res) => {
   const { username, fullname, email, password } = req.body;
 
@@ -36,4 +39,50 @@ const registerUser = async (req, res) => {
       .json(new ApiError(500, "User not stored successfully"));
   }
 };
-export { registerUser };
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new ApiError(400, "Invalid email or password");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new ApiError(400, "Invalid email or password");
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      },
+    );
+
+    const cookieOptions = {
+      httpOnly: true,
+    };
+
+    res.cookie("token", token, cookieOptions);
+
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(200, token, { message: "User logged-in successfully" }),
+      );
+  } catch (error) {
+    return res.status(400).json(new ApiError(400, "Login failed"));
+  }
+};
+export { registerUser, loginUser };
