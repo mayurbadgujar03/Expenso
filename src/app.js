77e0077df22cd4fe220edcecd09f7e72feb37ad8
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-02 20:22:04 +0530

    register controller and router added

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+//router imports
+import userRegister from "./routes/auth.routes.js";
+
+app.use("/api/v1/register-user", userRegister);
+
 export default app;
