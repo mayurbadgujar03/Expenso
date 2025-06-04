398e08659881e3fb7b5e4428b6c402352955e764
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-04 15:59:59 +0530

    Auth middleware added in src/middlewares/

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+import { ApiError } from "../utils/api-error.js";
+
+dotenv.config();
+
+const isLoggedIn = async (req, res, next) => {
+  const token = req.cookie?.token;
+
+  try {
+    if (!token) {
+      throw new ApiError(400, "Authentication failed");
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(500).json(new ApiError(500, "Internal server error"));
+  }
+};
+
+export default isLoggedIn;
