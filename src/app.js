7f647f99993eba5634a3668fd354d47e88bd4315
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-04 15:25:44 +0530

    CORS and URL encoding added

--- src/app.js
+++ src/app.js
@@ -8,7 +8,16 @@ import userRegister from "./routes/auth.routes.js";
 import loginUser from "./routes/auth.routes.js";
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/api/v1/users", userRegister);
 app.use("/api/v1/users", loginUser);
