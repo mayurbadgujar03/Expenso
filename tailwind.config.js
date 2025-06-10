ff00c25e2391de72723f82724f0b582dbcd75573
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-11 00:56:06 +0530

    refactor: Hero colors, How it works and moving testimonials

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+  content: ["./public/**/*.html", "./**/*.html"],
+  safelist: [
+    "bg-gradient-to-r",
+    "from-indigo-500",
+    "to-purple-600",
+    "text-white",
+    "text-gray-100",
+    "bg-[#007bff]",
+    "bg-[#fd7e14]",
+  ],
+};
