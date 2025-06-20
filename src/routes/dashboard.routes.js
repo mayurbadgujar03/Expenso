d60ab7020b4196187b148516ba965fc57817914a
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-06-20 06:42:03 +0530

    feat(items): implement item deletion by ID

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -6,6 +6,7 @@ import {
   dashboard,
   confirm,
   updatePrice,
+  deleteItem,
 } from "../controllers/dashboard.controllers.js";
 
 const router = Router();
@@ -14,5 +15,6 @@ router.route("/items/create").post(isLoggedIn, createItem);
 router.route("/").get(isLoggedIn, dashboard);
 router.route("/dashboard/purchase/confirm").post(isLoggedIn, confirm);
 router.route("/dashboard/update-price/:item_id").put(isLoggedIn, updatePrice);
+router.route("/dashboard/delete-item/:item_id").delete(isLoggedIn, deleteItem);
 
 export default router;
