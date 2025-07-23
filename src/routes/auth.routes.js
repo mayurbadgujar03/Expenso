b85634c12cd0cba49f5814b28e1a14e243957c13
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-07-23 22:57:10 +0530

    Secured /register router with validators

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { registerUser, loginUser, logoutUser } from "../controllers/auth.controllers.js";
 import isLoggedIn from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegistrationValidator } from "../validators/auth.validators.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegistrationValidator(), validate, registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(isLoggedIn, logoutUser);
 
